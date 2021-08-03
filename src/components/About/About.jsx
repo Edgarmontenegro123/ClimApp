044505e25c52d3152325152f5589e667b71df887
Edgar Montenegro <edgarmontenegro321@gmail.com>
2021-08-03 13:00:13 -0300

    About corrected

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,17 +6,17 @@ export default function About(){
     return(  
         <>  
             <div className = 'container'>
-                <span className = 'text'>Sígueme en <a href = 'https://github.com/SergioSotelo95' target = '_blank' rel = 'noopener noreferrer' className = 'link'>
+                <span className = 'text'>Sígueme en <a href = 'https://github.com/Edgarmontenegro123' target = '_blank' rel = 'noopener noreferrer' className = 'link'>
                         GitHub <i className="fab fa-github"></i></a>   
                 </span>    
-                <span className = 'text'>Sígueme en <a href = 'https://www.linkedin.com/in/sergio-sotelo/' target = '_blank' rel = 'noopener noreferrer' className = 'link'> 
+                <span className = 'text'>Sígueme en <a href = 'https://www.linkedin.com/in/edgarmontenegro/' target = '_blank' rel = 'noopener noreferrer' className = 'link'> 
                     LinkedIn <i className = "fab fa-linkedin-in"></i></a>  
                 </span>
-                <p className = 'text'>Mi Currículum Vitae <a href = 'https://drive.google.com/file/d/1c7QAi3kJC0gj0Lv40xOvbYmeJ30p8co3/view?usp=sharing' 
+                <p className = 'text'>Mi Currículum Vitae <a href = 'https://www.canva.com/design/DAElFbGgReo/w5KZpX2OLMFUX0hXbxV56w/view?utm_content=DAElFbGgReo&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink' 
                                                         target = '_blank' rel = 'noopener noreferrer' className = 'link'> Acá!</a>
                 </p>
                 <p className = 'text'>Conoce el código 
-                    <a href = 'https://github.com/SergioSotelo95/Weather-App' target = '_blank' rel = 'noopener noreferrer' className = 'link'> Acá!</a>
+                    <a href = 'https://github.com/Edgarmontenegro123/ClimApp' target = '_blank' rel = 'noopener noreferrer' className = 'link'> Acá!</a>
                 </p>
             </div>
             <div className = 'footer'>
